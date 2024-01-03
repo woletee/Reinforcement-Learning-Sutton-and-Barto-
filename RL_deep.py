10deef3fa0924ceb867ceafce06556d59c3e97b3
Mary Liyew <118590747+woletee@users.noreply.github.com>
2024-01-03 22:40:08 +0900

    Update RL_deep.py

--- RL_deep.py
+++ RL_deep.py
@@ -1,3 +1,11 @@
 #importing the necessary libraries 
 import numpy as np
 import matplotlib.pyplot as plt
+#next prepare the data
+
+x = np.array([[0, 1], [1, 0], [1, 1], [0, 0]])
+y = np.array([[1], [1], [0], [0]])
+#next is to define the number of nodes in each layer 
+num_input=2
+num_hidden=5
+num_output=1
