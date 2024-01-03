cf77f0efd5efe4ffd0e9137d8fba102a1908dd79
Mary Liyew <118590747+woletee@users.noreply.github.com>
2024-01-03 22:40:22 +0900

    Update RL_deep.py

--- RL_deep.py
+++ RL_deep.py
@@ -9,3 +9,13 @@ y = np.array([[1], [1], [0], [0]])
 num_input=2
 num_hidden=5
 num_output=1
+#we will intitalize the weights and the bias randomly as follows
+weight_ih=np.random.randn(num_input, num_hidden)
+bias_ih=np.zeros((1,num_hidden))
+#next we will initalize weight and bias from the input to the output layer
+weight_ho=np.random.randn(num_hidden, num_output)
+bias_ho=np.zeros((1,num_output))
+#next we will define the sigmoid activation function 
+def sigmoid(z):
+    return 1/(1+np.exp(-z))
+#next we will define the derivative of the sigmoid function
